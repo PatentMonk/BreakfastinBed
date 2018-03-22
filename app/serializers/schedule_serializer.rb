30b70381464c2d005bdf60f58213c3942a06c03f
Jordan Rushing <puait02@gmail.com>
2018-03-21 23:23:39 -0500

    Set schedule attributes for its serializer

--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -1,3 +1,6 @@
 class ScheduleSerializer < BaseSerializer
-  attributes :
+  attributes :monday_opens_at, :monday_closes_at, :tuesday_opens_at, :tuesday_closes_at,
+             :wednesday_opens_at, :wednesday_closes_at, :thursday_opens_at, :thursday_closes_at,
+             :friday_opens_at, :friday_closes_at, :saturday_opens_at, :saturday_closes_at,
+             :sunday_opens_at, :sunday_closes_at
 end
