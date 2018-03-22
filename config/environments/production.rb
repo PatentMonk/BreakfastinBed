1aa0ae10f031285a57617719bc38f4394393e42f
Jordan Rushing <puait02@gmail.com>
2018-03-21 23:36:32 -0500

    Work on getting email set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,16 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'wakeandbreak.heroku.com',
+    :authentication => :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
