4eb410276dcaacf20a01eee28fde177ebe5a8c94
Jordan Rushing <puait02@gmail.com>
2018-03-22 22:43:11 -0500

    Secure changes

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,7 +3,6 @@ module Api
     include Response
     include ExceptionHandler
 
-    # called before every action on controllers
     before_action :authorize_request
     attr_reader :current_user
 
--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -1,9 +1,3 @@
-# Be sure to restart your server when you modify this file.
-#
-# This file contains migration options to ease your Rails 5.0 upgrade.
-#
-# Read the Guide for Upgrading Ruby on Rails for more info on each option.
-
 Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
 
 # Enable per-form CSRF tokens. Previous versions had false.
